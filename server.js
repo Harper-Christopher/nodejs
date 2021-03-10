feb50157416a102d0a547f20d102e5847e1000f2
Harper-Christopher <christopherharper123@gmail.com>
2021-03-09 22:24:56 -0700

    server.js update

--- server.js
+++ server.js
@@ -1,6 +1,16 @@
+const express = require('express');
+const app = express();
+const {pool} = require("pg");
 require('dotenv').config();
 const connectionString = process.env.DATABASE_URL;
 const pool = new Pool({connectionString: connectionString});
+
+app.set('port', (process.env.PORT || 5000));
+app.use(express.static(__dirname + '/public'));
+
+app.listen(app.get('port'), function() {
+    console.log('App running on port', app.get('port'));
+});
 
 var sql = "SELECT * FROM guitar";
 
