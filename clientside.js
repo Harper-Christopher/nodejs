d602b9468e1b3f5c25a5f0e6dc34669ed972264c
Harper-Christopher <christopherharper123@gmail.com>
2021-03-20 16:58:06 -0600

    ajax update

---
+++ clientside.js
@@ -0,0 +1,22 @@
+// function getResults() {
+//     console.log("Requesting orders clientside from orders page");
+
+//     let userid = $("#userid").val();
+//     console.log("#userid " + userid), function(data) {
+//         console.log("Back form the server with:");
+//         console.log(data);
+//     }
+// }
+
+// function getResults() {
+//     $.ajax({
+//         type: 'POST',
+//         url: 'http://localhost:5050/orderDetail',
+//         success: function(response) {
+//             console.log(response);
+//         },
+//         error: function(xhr, status, err) {
+//             console.log(xhr.responseText);
+//         }
+//     });
+// }
--- server.js
+++ server.js
@@ -3,6 +3,12 @@ const app = express();
 process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0"
 const { Pool } = require("pg");
 require('dotenv').config();
+const bodyParser = require('body-parser');
+const XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
+const xhr = new XMLHttpRequest();
+// const jsdom = require("jsdom");
+// const { JSDOM } = jsdom;
+// global.document = new JSDOM(html).window.document;
 const connectionString = process.env.DATABASE_URL;
 const pool = new Pool(
     {connectionString: connectionString,
@@ -15,6 +21,7 @@ app.set('port', (process.env.PORT || 5050));
 app.use(express.static(__dirname + '/public'));
 app.set("views", "views");
 app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({ extended: true })); 
 
 app.listen(app.get('port'), function() {
     console.log('App running on port', app.get('port'));
@@ -24,16 +31,56 @@ app.listen(app.get('port'), function() {
 
 //------Controller------//
 app.post("/guitars", function(req, res) {
-    console.log("Received a request for the home page");
+    console.log("Received a request for the guitar page");
      getGuitar1(res);
-    // const name = getCurrentLoggedInUserAccount();
-    // var params = {guitar1: guitar1, guitar2: guitar2, guitar3: guitar3, guitar4: guitar4, guitar5: guitar5, guitar6: guitar6, username: name};
 
-    // console.log(name);
-    // console.log(guitar1);
-
-    // res.render("guitars", params);
 });
+
+app.get("/orders", function(req, res) {
+    console.log("Received a request for the order page");
+    res.render("orders");
+})
+
+app.post("/orders", function(req, res) {
+    console.log("Received a request for the post orders page");
+    var customerName = req.body.customer;
+    // var customerOrders = getCustomerOrder(customerName);
+  
+    console.log("Customer number is " + customerName);
+
+    // renderHomePage(name);
+    res.render("orders");
+  })
+
+app.post("/addOrder", function(req, res) {
+    console.log("Received a request for the add order page");
+    var customer = req.body.customer;
+    var guitar = req.body.guitar1;
+    var price = req.body.price1;
+
+    var params = {guitar: guitar, price: price, customer: customer};
+
+    res.render("addOrder", params);
+})
+
+app.post("/orderDetail", function(req, res) {
+    console.log("Received a request to display orders");
+    var userid = req.body.userid;
+    // let userid = req.query.userid;
+    console.log(userid);
+    guitarOrders(res, userid);
+})
+
+// xhr.open("POST", "/orderDetail");
+// xhr.onreadystatechange = function(req, res) {
+//     if (this.readyState == 4 && this.status == 200) {
+//     console.log("Received a request to display orders");
+//     var userid = req.body.userid;
+//     console.log(userid);
+//     guitarOrders(res, userid);
+//     }
+// }
+
 
 
 
@@ -41,6 +88,7 @@ app.post("/guitars", function(req, res) {
 // function getCurrentLoggedInUserAccount() {
 //     return "Christopher";
 //   }
+
 
 function getGuitar1(res) {
 var sql = "SELECT * FROM guitar WHERE guitarid <= 6";
@@ -62,7 +110,41 @@ pool.query(sql, function(err, result) {
 
 const name = "Christopher";
 
+
     res.render("guitars", {info: detail, name: name});
 
 }); 
 }  
+
+
+
+function guitarOrders(res, userid) {
+    var sql = "SELECT guitar.guitarname, guitar.price, orders.userid FROM guitar INNER JOIN orders ON guitar.guitarid=orders.guitarid WHERE userid =" + userid;
+    // var sql = "SELECT * FROM customer";
+    
+    pool.query(sql, function(err, result) {
+        // If an error occurred...
+        if (err) {
+            console.log("Error in query: ")
+            console.log(err);
+        }
+    
+        // Log this to the console for debugging purposes.
+        console.log("Back from DB with result:");
+        console.log(sql);
+        console.log(result.rows);
+        console.log(userid);
+        
+        var data = JSON.stringify(result.rows);
+        var detail = JSON.parse(data);
+    
+    const name = "Christopher";
+    
+        console.log(detail);
+        // let resultList = document.querySelector('allOrders');
+        // resultList.append(detail);
+        // $("#allOrders").append(detail);
+        res.render("ordersPage", {info: detail, name: name});
+        
+    }); 
+    }  
